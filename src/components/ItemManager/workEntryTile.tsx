9595f7a6be98bd9740d99901f009b3dded15e89f
ToCans <thomas.cansfield@gmail.com>
2025-11-16 13:21:23 +0100

    Additional Work Entry Tile tweak needed.

--- src/components/ItemManager/workEntryTile.tsx
+++ src/components/ItemManager/workEntryTile.tsx
@@ -42,16 +42,19 @@ const WorkEntryTile = ({ workEntry, workTasks, workTopics }: WorkEntryTileProps)
 			<div className='flex flex-row space-x-2 items-center w-full p-0.5'>
 				<ColorIcon color={tileColor} />
 				<div className='flex flex-row justify-between items-center w-full'>
-					<div className='flex md:flex-row flex-col md:w-3/5 md:items-center'>
-						<p className='text-sm text-nowrap md:w-2/5'>{tileTaskName}</p>
-						<p className='text-sm text-nowrap md:w-2/5'>{tileTopicName}</p>
+					<div className='flex md:flex-row flex-col md:w-3/5 w-1/2 md:items-center'>
+						<p className='text-sm text-nowrap md:w-2/5 w-full'>{tileTaskName}</p>
+						<p className='text-sm text-nowrap md:w-2/5 w-full'>{tileTopicName}</p>
 					</div>
-					<div className='flex flex-row md:w-2/5 justify-between'>
+					<div className='flex flex-row md:w-2/5 w-1/2 justify-between items-center'>
 						<p className='text-gray-500 text-sm'>{formatMinutes(workEntry.duration)}</p>
-						<div className='flex md:flex-row flex-col md:space-x-1 md:items-center'>
+						<div className='flex md:flex-row flex-col md:space-x-1 items-center'>
 							<p className='text-gray-500 text-sm'>{date}</p>
-							<p className='text-gray-500 text-sm'>@</p>
-							<p className='text-gray-500 text-sm'>{time}</p>
+							<div className='flex flex-row space-x-1'>
+								<p className='text-gray-500 text-sm'>@</p>
+								<p className='text-gray-500 text-sm'>{time}</p>
+							</div>
+
 						</div></div>
 
 				</div>
